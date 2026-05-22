1fbdf28386121bbb47eb0d778606f3d7a6d573f1
DevOfSomewhere <68583362+SunvyWasTaken@users.noreply.github.com>
2026-05-22 10:27:38 +0200

    UpdatePath

---
+++ Engine/SunsetEngine.h
@@ -0,0 +1,11 @@
+//
+// Created by sunvy on 22/05/2026.
+//
+
+#ifndef SUNSETCRAFT_V2_SUNSETENGINE_H
+#define SUNSETCRAFT_V2_SUNSETENGINE_H
+
+#include "Core/Application.h"
+#include "Core/ApplicationSetting.h"
+
+#endif //SUNSETCRAFT_V2_SUNSETENGINE_H
