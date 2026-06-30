c3452523bd565369d25b9707ffd0a158e22381ca
DevOfSomewhere <68583362+SunvyWasTaken@users.noreply.github.com>
2026-06-30 20:49:01 +0200

    Prepare for FBO

--- Thirdparty/SRmGUI/Sources/Image.cpp
+++ Thirdparty/SRmGUI/Sources/Image.cpp
@@ -21,7 +21,7 @@ namespace SRmGUI
     void Image::Paint(FormeDatas &out)
     {
         Widget::Paint(out);
-        out.emplace_back(Forme::Image{{m_DesireParameter.Position, m_DesireParameter.Size, {1.f, 1.f, 1.f, 1.f}}, m_TextureID});
+        out.emplace_back(Forme::Image{{m_DesireParameter.Position, m_DesireParameter.Size, {1.f, 1.f, 1.f, 1.f}}, m_TextureID, m_Uv});
     }
 
     void Image::SetImage(TextureID id)
