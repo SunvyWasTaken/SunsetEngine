7f7f043b3da366bb366b4e15f7aeb9bac83f3ec6
DevOfSomewhere <68583362+SunvyWasTaken@users.noreply.github.com>
2026-07-27 21:21:31 +0200

    Remove try catch :/

--- Game/main.cpp
+++ Game/main.cpp
@@ -32,27 +32,19 @@ namespace
 
 int main()
 {
-    try
-    {
-        Sunset::WindowSetting setting{"SunsetCraft"};
-        setting.GraphicsAPI = SelectGraphicsAPI();
+    Sunset::WindowSetting setting{"SunsetCraft"};
+    setting.GraphicsAPI = SelectGraphicsAPI();
 
-        Sunset::Application app{setting};
-        app.SetWindow(std::make_unique<Sunset::GLFWWindow>(setting));
+    Sunset::Application app{setting};
+    app.SetWindow(std::make_unique<Sunset::GLFWWindow>(setting));
 #ifdef SUNSET_VULKAN
-        Sunset::RenderCommand::SetRenderAPI(std::make_unique<Sunset::VulkanGraphicsDevice>());
+    Sunset::RenderCommand::SetRenderAPI(std::make_unique<Sunset::VulkanGraphicsDevice>());
 #elif defined(SUNSET_OPENGL)
-        Sunset::RenderCommand::SetRenderAPI(std::make_unique<Sunset::OpenGLGraphicsDevice>());
+    Sunset::RenderCommand::SetRenderAPI(std::make_unique<Sunset::OpenGLGraphicsDevice>());
 #endif
-        Sunset::GameModuleLoader loader;
-        loader.Load(LOCAL_PATH  "SunsetCraft.so", app);
-        app.Run();
-        loader.Unload();
-        return EXIT_SUCCESS;
-    }
-    catch (const std::exception& exception)
-    {
-        std::cerr << "[Fatal] " << exception.what() << '\n';
-        return EXIT_FAILURE;
-    }
+    Sunset::GameModuleLoader loader;
+    loader.Load(LOCAL_PATH  "SunsetCraft.so", app);
+    app.Run();
+    loader.Unload();
+    return EXIT_SUCCESS;
 }
