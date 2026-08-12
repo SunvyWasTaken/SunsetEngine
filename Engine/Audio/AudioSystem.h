7413ed0da306f852f95f88f012a7e8d83f69d759
DevOfSomewhere <68583362+SunvyWasTaken@users.noreply.github.com>
2026-08-12 23:32:28 +0200

    Play music (ultracombo from LOL 2019).

--- Engine/Audio/AudioSystem.h
+++ Engine/Audio/AudioSystem.h
@@ -6,9 +6,17 @@
 
 namespace Sunset
 {
+    class AudioSource;
+    class AudioBuffer;
+
     struct AudioSystem
     {
+        friend class Application;
+    private:
         static void Init();
         static void Shutdown();
+    public:
+        static std::shared_ptr<AudioBuffer> CreateAudioBuffer(const std::filesystem::path& path);
+        static AudioSource* Play2DAudio(const std::shared_ptr<AudioBuffer>& audio, bool loop);
     };
 } // Sunset
