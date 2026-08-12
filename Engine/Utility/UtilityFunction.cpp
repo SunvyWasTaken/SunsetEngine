a0919f8c4856837aff2acc86e4dd5a3e8830a978
DevOfSomewhere <68583362+SunvyWasTaken@users.noreply.github.com>
2026-08-12 10:00:03 +0200

    Clean useless Json

--- Engine/Utility/UtilityFunction.cpp
+++ Engine/Utility/UtilityFunction.cpp
@@ -13,34 +13,34 @@ bool Sunset::UtilityFunction::DoesFileExist(const std::string_view &path)
     return std::filesystem::exists(path);
 }
 
-bool Sunset::UtilityFunction::LoadJson(const std::string_view& path, nlohmann::json& json)
-{
-    std::ifstream file;
-    file.open(path.data());
-
-    if (!file.is_open())
-    {
-        LOG("Engine", error, "Json file couldn't be open : {}", path);
-        DEBUG_BREAK();
-        return false;
-    }
-
-    file >> json;
-    return true;
-}
-
-bool Sunset::UtilityFunction::SaveJson(const std::string_view &path, const nlohmann::json &json = nlohmann::json::object())
-{
-    CREATE_PATH(path);
-
-    std::ofstream file(path.data());
-    if (!file.is_open())
-    {
-        LOG("Engine", error,
-            "Couldn't create json file : {}", path);
-        return false;
-    }
-    file << json.dump();
-    file.close();
-    return true;
-}
+// bool Sunset::UtilityFunction::LoadJson(const std::string_view& path, nlohmann::json& json)
+// {
+//     std::ifstream file;
+//     file.open(path.data());
+//
+//     if (!file.is_open())
+//     {
+//         LOG("Engine", error, "Json file couldn't be open : {}", path);
+//         DEBUG_BREAK();
+//         return false;
+//     }
+//
+//     file >> json;
+//     return true;
+// }
+//
+// bool Sunset::UtilityFunction::SaveJson(const std::string_view &path, const nlohmann::json &json = nlohmann::json::object())
+// {
+//     CREATE_PATH(path);
+//
+//     std::ofstream file(path.data());
+//     if (!file.is_open())
+//     {
+//         LOG("Engine", error,
+//             "Couldn't create json file : {}", path);
+//         return false;
+//     }
+//     file << json.dump();
+//     file.close();
+//     return true;
+// }
--- Engine/Utility/UtilityFunction.h
+++ Engine/Utility/UtilityFunction.h
@@ -4,14 +4,14 @@
 
 #pragma once
 
-#include <nlohmann/json.hpp>
+// #include <nlohmann/json.hpp>
 
 namespace Sunset
 {
     struct UtilityFunction
     {
         static bool DoesFileExist(const std::string_view &path);
-        static bool LoadJson(const std::string_view& path, nlohmann::json& json);
-        static bool SaveJson(const std::string_view& path, const nlohmann::json& json);
+        // static bool LoadJson(const std::string_view& path, nlohmann::json& json);
+        // static bool SaveJson(const std::string_view& path, const nlohmann::json& json);
     };
 }
